c5f4e5101203eb46e66effbf31a3c647fc092823
Wayne Lewis <wlewis@ospreydcs.com>
2022-05-18 08:38:59 -0700

    Change to table setup

--- iocBoot/ioccs_tbl/st.cmd
+++ iocBoot/ioccs_tbl/st.cmd
@@ -9,7 +9,7 @@ epicsEnvSet("ENGINEER", "Engineer")
 epicsEnvSet("LOCATION", "Location")
 
 epicsEnvSet("AS_PATH", "${TOP}/autosave")
-epicsEnvSet("IOC_PREFIX","MCT:CS_Y_VBM")
+epicsEnvSet("IOC_PREFIX","MCT:CS_Y_TBL1")
 
 cd ${TOP}
 
@@ -18,8 +18,8 @@ dbLoadDatabase("dbd/cs_tbl.dbd",0,0)
 cs_tbl_registerRecordDeviceDriver(pdbbase) 
 
 ## Load record instances
-dbLoadRecords("db/3jack_mirror.db","P=MCT:VBM:,Q=CS_Y:,M1=US_OB,M2=US_IB,M3=DS,LENGTH=1070,WIDTH=210")
-dbLoadRecords("db/3jack_mirror_cs_motor.db","P=MCT:VBM:,Q=CS_Y:,M1=US_OB,M2=US_IB,M3=DS")
+dbLoadRecords("db/2jack_table.db","P=MCT:TBL1:,Q=CS_Y:,M1=US,M2=DS,LENGTH=2000")
+dbLoadRecords("db/2jack_table_cs_motor.db","P=MCT:TBL1:,Q=CS_Y:,M1=US,M2=DS")
 dbLoadRecords("db/iocAdminSoft.db", "IOC=$(IOC_PREFIX)")
 dbLoadRecords("db/save_restoreStatus.db", "P=$(IOC_PREFIX):")
 
